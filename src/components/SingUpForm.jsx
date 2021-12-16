bd2f1b4514916bb173ecace9cd463d1d9261922e
Zahra Nejati <nejatizahra4@gmail.com>
2021-12-16 18:57:39 +0330

    set user data

--- src/components/SingUpForm.jsx
+++ src/components/SingUpForm.jsx
@@ -1,20 +1,49 @@
+import { useState } from "react";
+
 const SingUpForm = () => {
+  const [inputform, setInputForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const changeHandler = (e) => {
+    setInputForm({ ...inputform, [e.target.name]: e.target.value });
+    console.log(inputform);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="container">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="formControl">
           <label htmlFor="name">Name</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={inputform.name}
+            name="name"
+          />
         </div>
         <div className="formControl">
           <label htmlFor="email">Email</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={inputform.email}
+            name="email"
+          />
         </div>
         <div className="formControl">
           <label htmlFor="password">Password</label>
-          <input type="text" />
+          <input
+            name="password"
+            type="text"
+            onChange={changeHandler}
+            value={inputform.password}
+          />
         </div>
-        <button>submit</button>
+        <button type="submit">submit</button>
       </form>
     </div>
   );
